37bf9d02f094112cdd993a04577923fe758211fb
1 <wrasf175@naver.com>
2023-01-26 22:25:09 +0900

    버튼 비활성화 테스트 코드 작성

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -38,3 +38,34 @@ it('Checkbox disables button on first click and enables on second click', () => 
   fireEvent.click(checkbox);
   expect(button).toBeEnabled();
 });
+
+it('Disabled button has gray background and reverts to red', () => {
+  render(<App />);
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button' });
+  const button = screen.getByRole('button');
+
+  // disable button
+  fireEvent.click(checkbox);
+  expect(button).toHaveStyle('background-color: gray');
+
+  // re-enable button
+  fireEvent.click(checkbox);
+  expect(button).toHaveStyle('background-color: red');
+});
+
+it('Clicked disabled button has gray background and reverts to blue', () => {
+  render(<App />);
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button' });
+  const button = screen.getByRole('button');
+
+  // change button to blue
+  fireEvent.click(button);
+
+  // disable button
+  fireEvent.click(checkbox);
+  expect(button).toHaveStyle('background-color: gray');
+
+  // re-enable button
+  fireEvent.click(checkbox);
+  expect(button).toHaveStyle('background-color: blue');
+} );
--- tokentest/src/App.js
+++ tokentest/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const [ data, setData ] = useState(null);
+  const oAuthURL = 
+  `https://accounts.google.com/o/oauth2/v2/auth?client_id=946916753815-sme09oj9iqieo2lbf6837j4qdp3h50s6.apps.googleusercontent.com&response_type=token&redirect_uri=http://localhost:3000&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid`
+  const oAuthHandler = () => {
+    window.location.assign(oAuthURL);
+  }
+
+  useEffect( () => {
+    const url = new URL(window.location.href);
+    const hash = url.hash;
+
+    async function fetchData() {
+      if (hash) {
+      const accessToken = hash?.split("=")[1].split("&")[0];
+      await axios.get('https://www.googleapis.com/oauth2/v2/userinfo?access_token=' + accessToken, { 
+        headers: { 
+          authorization: `token ${accessToken}`, 
+          accept: 'application/json' 
+        }})
+        .then(data => {
+          setData(data);
+          console.log(data.data);
+      }).catch(e => console.log('oAuth token expired'));
+
+      console.log(accessToken);
+    }
+  }
+    fetchData();
+
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button id="oAuthBtn" onClick={oAuthHandler}>
+        <div id="comment">구글 OAuth</div>
+      </button>
     </div>
   );
 }
