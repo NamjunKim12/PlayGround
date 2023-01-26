ec38e2bad9e5c9942296c1b0fcc5b370ada8be94
1 <wrasf175@naver.com>
2023-01-26 21:39:38 +0900

    버튼 클릭 테스트

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // find an element with a role of button and text of 'Change to blue'
+  const ColorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  // expect the background color to be red
+  expect(ColorButton).toHaveStyle({ backgroundColor: 'red' });
+
+  // click button
+  fireEvent.click(ColorButton);
+  expect(ColorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  expect(ColorButton).toHaveTextContent('Change to red');
 });
